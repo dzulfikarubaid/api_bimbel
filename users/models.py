d0a28c8906d3afc90a36cd4c9295b21a9b7ddecc
Ahmad Dzulfikar Ubaidillah <dzulfprojects@gmail.com>
2023-05-14 09:06:03 +0700

    changes

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class modelOrtu(models.Model):
     nama = models.CharField(max_length=100)
     alamat = models.CharField(max_length=250)
     jenis_kelamin = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
     nomor_telepon = models.CharField(max_length=50)
 
     def __str__(self):
